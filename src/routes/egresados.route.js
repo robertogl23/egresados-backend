5c5a1d61ece9a94abddfc0a65c24b9390ef493b4
Ramses <ecologiaramses62@gmail.com>
2021-03-09 21:59:57 -0600

    cambios

--- src/routes/egresados.route.js
+++ src/routes/egresados.route.js
@@ -108,6 +108,7 @@ router.get('/perfil', async (req, res) => {
             const data5 = await run(client,'F5:F')
             const data6 = await run(client,'G5:G')
             const data7 = await run(client,'H5:H')
+           
             
             return res.json({
                 correos:data.flat(),
@@ -136,6 +137,32 @@ router.get('/data/dashboard', async (req, res) => {
         }else{
             const data2 = await run(client,'L2:L')
             const data3 = await run(client,'P2:P')
+            const data10 = await run(client,'AE2:AE')
+            const data11 = await run(client,'AH2:AH')
+            const data12 = await run(client,'AI2:AI')
+            const data13 = await run(client,'AJ2:AJ')
+            const data14 = await run(client,'AK2:AK')
+            const data15 = await run(client,'AL2:AL')
+            const data16 = await run(client,'AM2:AM')
+            const data17 = await run(client,'AN2:AN')
+            const data18 = await run(client,'AO2:AO')
+            const data19 = await run(client,'AP2:AP')
+            const data20 = await run(client,'AQ2:AQ')
+            const data21 = await run(client,'AR2:AR')
+            const data22 = await run(client,'AT2:AT')
+            const data23 = await run(client,'AU2:AU')
+            const data24 = await run(client,'AV2:AV')
+            const data25 = await run(client,'AW2:AW')
+            const data26 = await run(client,'AX2:AX')
+            const data27 = await run(client,'AY2:AY')
+            const data28 = await run(client,'AZ2:AZ')
+            const data29 = await run(client,'BA2:BA')
+            const data30 = await run(client,'BB2:BB')
+            const data31 = await run(client,'BC2:BC')
+            const data32 = await run(client,'BD2:BD')
+            const data33 = await run(client,'BL2:BL')
+            const data34 = await run(client,'BS2:BS')
+            const data35 = await run(client,'BY2:BY')
             const data4 = await run(client,'Q2:Q')
             console.log(data4)
             const fechasOrdenadas = Array.from(new Set(data4.flat())).sort()
@@ -163,8 +190,209 @@ router.get('/data/dashboard', async (req, res) => {
                     ["Si",getDataEstadistica("Si",data3)],
                     ["No",getDataEstadistica("No",data3)],
                 ],
-                dateTitulacion:[titulacionDate()]
+                dateTitulacion:[titulacionDate()],
 
+                contenidoRelevante:[
+                    ["Name", "¿Los contenidos de las materias fueron utiles?"],
+                    ["Muy de acuerdo",getDataEstadistica("Muy de acuerdo",data10)],
+                    ["De acuerdo",getDataEstadistica("De acuerdo",data10)],
+                    ["En desacuerdo",getDataEstadistica("En desacuerdo",data10)],
+                    ["Muy en desacuerdo",getDataEstadistica("Muy en desacuerdo",data10)],
+                ],
+
+                comunicacion:[
+                    ["Name", "Nivel de Comunicacion del Egresado"],
+                    ["EXCELENTE (95% A 100%)",getDataEstadistica("EXCELENTE (95% A 100%)",data11)],
+                    ["NOTABLE (90% A 94%)",getDataEstadistica("NOTABLE (90% A 94%)",data11)],
+                    ["BUENO (80% A 89%)",getDataEstadistica("BUENO (80% A 89%)",data11)],
+                    ["SUFICIENTE (70% A 79%)",getDataEstadistica("SUFICIENTE (70% A 79%)",data11)],
+                    ["DEFICIENTE (MENOS DEL 70%)",getDataEstadistica("DEFICIENTE (MENOS DEL 70%)",data11)],
+                ],
+                pensamientoCritico:[
+                    ["Name", "Nivel del Pensamiento Critico del Egresado"],
+                    ["EXCELENTE (95% A 100%)",getDataEstadistica("EXCELENTE (95% A 100%)",data12)],
+                    ["NOTABLE (90% A 94%)",getDataEstadistica("NOTABLE (90% A 94%)",data12)],
+                    ["BUENO (80% A 89%)",getDataEstadistica("BUENO (80% A 89%)",data12)],
+                    ["SUFICIENTE (70% A 79%)",getDataEstadistica("SUFICIENTE (70% A 79%)",data12)],
+                    ["DEFICIENTE (MENOS DEL 70%)",getDataEstadistica("DEFICIENTE (MENOS DEL 70%)",data12)],
+                ],
+                solucionDeProblemas:[
+                    ["Name", "Nivel para identificar problemas del Egresado"],
+                    ["EXCELENTE (95% A 100%)",getDataEstadistica("EXCELENTE (95% A 100%)",data13)],
+                    ["NOTABLE (90% A 94%)",getDataEstadistica("NOTABLE (90% A 94%)",data13)],
+                    ["BUENO (80% A 89%)",getDataEstadistica("BUENO (80% A 89%)",data13)],
+                    ["SUFICIENTE (70% A 79%)",getDataEstadistica("SUFICIENTE (70% A 79%)",data13)],
+                    ["DEFICIENTE (MENOS DEL 70%)",getDataEstadistica("DEFICIENTE (MENOS DEL 70%)",data13)],
+                ],
+                interaccionSocial:[
+                    ["Name", "Nivel para formar parte de equipos de trabajo del Egresado"],
+                    ["EXCELENTE (95% A 100%)",getDataEstadistica("EXCELENTE (95% A 100%)",data14)],
+                    ["NOTABLE (90% A 94%)",getDataEstadistica("NOTABLE (90% A 94%)",data14)],
+                    ["BUENO (80% A 89%)",getDataEstadistica("BUENO (80% A 89%)",data14)],
+                    ["SUFICIENTE (70% A 79%)",getDataEstadistica("SUFICIENTE (70% A 79%)",data14)],
+                    ["DEFICIENTE (MENOS DEL 70%)",getDataEstadistica("DEFICIENTE (MENOS DEL 70%)",data14)],
+                ],
+                aprendizaje:[
+                    ["Name", "Nivel de aprendizaje del Egresado"],
+                    ["EXCELENTE (95% A 100%)",getDataEstadistica("EXCELENTE (95% A 100%)",data15)],
+                    ["NOTABLE (90% A 94%)",getDataEstadistica("NOTABLE (90% A 94%)",data15)],
+                    ["BUENO (80% A 89%)",getDataEstadistica("BUENO (80% A 89%)",data15)],
+                    ["SUFICIENTE (70% A 79%)",getDataEstadistica("SUFICIENTE (70% A 79%)",data15)],
+                    ["DEFICIENTE (MENOS DEL 70%)",getDataEstadistica("DEFICIENTE (MENOS DEL 70%)",data15)],
+                ],
+                consistenciaEtica:[
+                    ["Name", "Nivel para asumir principios éticos del Egresado"],
+                    ["EXCELENTE (95% A 100%)",getDataEstadistica("EXCELENTE (95% A 100%)",data16)],
+                    ["NOTABLE (90% A 94%)",getDataEstadistica("NOTABLE (90% A 94%)",data16)],
+                    ["BUENO (80% A 89%)",getDataEstadistica("BUENO (80% A 89%)",data16)],
+                    ["SUFICIENTE (70% A 79%)",getDataEstadistica("SUFICIENTE (70% A 79%)",data16)],
+                    ["DEFICIENTE (MENOS DEL 70%)",getDataEstadistica("DEFICIENTE (MENOS DEL 70%)",data16)],
+                ],
+                globalizado:[
+                    ["Name", "Nivel para comprender los aspectos interdependientes del mundo globalizado del Egresado"],
+                    ["EXCELENTE (95% A 100%)",getDataEstadistica("EXCELENTE (95% A 100%)",data17)],
+                    ["NOTABLE (90% A 94%)",getDataEstadistica("NOTABLE (90% A 94%)",data17)],
+                    ["BUENO (80% A 89%)",getDataEstadistica("BUENO (80% A 89%)",data17)],
+                    ["SUFICIENTE (70% A 79%)",getDataEstadistica("SUFICIENTE (70% A 79%)",data17)],
+                    ["DEFICIENTE (MENOS DEL 70%)",getDataEstadistica("DEFICIENTE (MENOS DEL 70%)",data17)],
+                ],
+                ciudadana:[
+                    ["Name", "Nivel para integrarse a la comunidad del Egresado"],
+                    ["EXCELENTE (95% A 100%)",getDataEstadistica("EXCELENTE (95% A 100%)",data18)],
+                    ["NOTABLE (90% A 94%)",getDataEstadistica("NOTABLE (90% A 94%)",data18)],
+                    ["BUENO (80% A 89%)",getDataEstadistica("BUENO (80% A 89%)",data18)],
+                    ["SUFICIENTE (70% A 79%)",getDataEstadistica("SUFICIENTE (70% A 79%)",data18)],
+                    ["DEFICIENTE (MENOS DEL 70%)",getDataEstadistica("DEFICIENTE (MENOS DEL 70%)",data18)],
+                ],
+                sensibilidadEstetica:[
+                    ["Name", "Nivel de apreciar y valorar diversas formas artísticas del Egresado"],
+                    ["EXCELENTE (95% A 100%)",getDataEstadistica("EXCELENTE (95% A 100%)",data19)],
+                    ["NOTABLE (90% A 94%)",getDataEstadistica("NOTABLE (90% A 94%)",data19)],
+                    ["BUENO (80% A 89%)",getDataEstadistica("BUENO (80% A 89%)",data19)],
+                    ["SUFICIENTE (70% A 79%)",getDataEstadistica("SUFICIENTE (70% A 79%)",data19)],
+                    ["DEFICIENTE (MENOS DEL 70%)",getDataEstadistica("DEFICIENTE (MENOS DEL 70%)",data19)],
+                ],
+                diseñar:[
+                    ["Name", "Nivel para diseñar, configurar y administrar redes computacionales del Egresado"],
+                    ["EXCELENTE (95% A 100%)",getDataEstadistica("EXCELENTE (95% A 100%)",data20)],
+                    ["NOTABLE (90% A 94%)",getDataEstadistica("NOTABLE (90% A 94%)",data20)],
+                    ["BUENO (80% A 89%)",getDataEstadistica("BUENO (80% A 89%)",data20)],
+                    ["SUFICIENTE (70% A 79%)",getDataEstadistica("SUFICIENTE (70% A 79%)",data20)],
+                    ["DEFICIENTE (MENOS DEL 70%)",getDataEstadistica("DEFICIENTE (MENOS DEL 70%)",data20)],
+                ],
+                diseñar2:[
+                    ["Name", "Nivel para diseñar, desarrollar y aplica modelos computacionales para solucionar problemas del Egresado"],
+                    ["EXCELENTE (95% A 100%)",getDataEstadistica("EXCELENTE (95% A 100%)",data21)],
+                    ["NOTABLE (90% A 94%)",getDataEstadistica("NOTABLE (90% A 94%)",data21)],
+                    ["BUENO (80% A 89%)",getDataEstadistica("BUENO (80% A 89%)",data21)],
+                    ["SUFICIENTE (70% A 79%)",getDataEstadistica("SUFICIENTE (70% A 79%)",data21)],
+                    ["DEFICIENTE (MENOS DEL 70%)",getDataEstadistica("DEFICIENTE (MENOS DEL 70%)",data21)],
+                ],
+                diseñar3:[
+                    ["Name", "Nivel para diseñar e implementar interfaces hombre-máquina y máquina-máquina para la automatización de sistemas del Egresado"],
+                    ["EXCELENTE (95% A 100%)",getDataEstadistica("EXCELENTE (95% A 100%)",data22)],
+                    ["NOTABLE (90% A 94%)",getDataEstadistica("NOTABLE (90% A 94%)",data22)],
+                    ["BUENO (80% A 89%)",getDataEstadistica("BUENO (80% A 89%)",data22)],
+                    ["SUFICIENTE (70% A 79%)",getDataEstadistica("SUFICIENTE (70% A 79%)",data22)],
+                    ["DEFICIENTE (MENOS DEL 70%)",getDataEstadistica("DEFICIENTE (MENOS DEL 70%)",data22)],
+                ],
+                identificarComprender:[
+                    ["Name", "Nivel para identificar y comprender las tecnologías de hardware del Egresado"],
+                    ["EXCELENTE (95% A 100%)",getDataEstadistica("EXCELENTE (95% A 100%)",data23)],
+                    ["NOTABLE (90% A 94%)",getDataEstadistica("NOTABLE (90% A 94%)",data23)],
+                    ["BUENO (80% A 89%)",getDataEstadistica("BUENO (80% A 89%)",data23)],
+                    ["SUFICIENTE (70% A 79%)",getDataEstadistica("SUFICIENTE (70% A 79%)",data23)],
+                    ["DEFICIENTE (MENOS DEL 70%)",getDataEstadistica("DEFICIENTE (MENOS DEL 70%)",data23)],
+                ],
+                diseñarBases:[
+                    ["Name", "Nivel para diseñar, desarrollar y administrar bases de datos conforme a requerimientos definidos del Egresado"],
+                    ["EXCELENTE (95% A 100%)",getDataEstadistica("EXCELENTE (95% A 100%)",data24)],
+                    ["NOTABLE (90% A 94%)",getDataEstadistica("NOTABLE (90% A 94%)",data24)],
+                    ["BUENO (80% A 89%)",getDataEstadistica("BUENO (80% A 89%)",data24)],
+                    ["SUFICIENTE (70% A 79%)",getDataEstadistica("SUFICIENTE (70% A 79%)",data24)],
+                    ["DEFICIENTE (MENOS DEL 70%)",getDataEstadistica("DEFICIENTE (MENOS DEL 70%)",data24)],
+                ],
+                soluciones:[
+                    ["Name", "Nivel para integrar soluciones computacionales con diferentes tecnologías del Egresado"],
+                    ["EXCELENTE (95% A 100%)",getDataEstadistica("EXCELENTE (95% A 100%)",data25)],
+                    ["NOTABLE (90% A 94%)",getDataEstadistica("NOTABLE (90% A 94%)",data25)],
+                    ["BUENO (80% A 89%)",getDataEstadistica("BUENO (80% A 89%)",data25)],
+                    ["SUFICIENTE (70% A 79%)",getDataEstadistica("SUFICIENTE (70% A 79%)",data25)],
+                    ["DEFICIENTE (MENOS DEL 70%)",getDataEstadistica("DEFICIENTE (MENOS DEL 70%)",data25)],
+                ],
+                empresarial:[
+                    ["Name", "Nivel para desarrollar una visión empresarial para detectar áreas de oportunidad del Egresado"],
+                    ["EXCELENTE (95% A 100%)",getDataEstadistica("EXCELENTE (95% A 100%)",data26)],
+                    ["NOTABLE (90% A 94%)",getDataEstadistica("NOTABLE (90% A 94%)",data26)],
+                    ["BUENO (80% A 89%)",getDataEstadistica("BUENO (80% A 89%)",data26)],
+                    ["SUFICIENTE (70% A 79%)",getDataEstadistica("SUFICIENTE (70% A 79%)",data26)],
+                    ["DEFICIENTE (MENOS DEL 70%)",getDataEstadistica("DEFICIENTE (MENOS DEL 70%)",data26)],
+                ],
+                desempeñar:[
+                    ["Name", "Nivel para desempeñar sus actividades profesionales del Egresado"],
+                    ["EXCELENTE (95% A 100%)",getDataEstadistica("EXCELENTE (95% A 100%)",data27)],
+                    ["NOTABLE (90% A 94%)",getDataEstadistica("NOTABLE (90% A 94%)",data27)],
+                    ["BUENO (80% A 89%)",getDataEstadistica("BUENO (80% A 89%)",data27)],
+                    ["SUFICIENTE (70% A 79%)",getDataEstadistica("SUFICIENTE (70% A 79%)",data27)],
+                    ["DEFICIENTE (MENOS DEL 70%)",getDataEstadistica("DEFICIENTE (MENOS DEL 70%)",data27)],
+                ],
+                habMetod:[
+                    ["Name", "Nivel para poseer habilidades metodológicas de investigación del Egresado"],
+                    ["EXCELENTE (95% A 100%)",getDataEstadistica("EXCELENTE (95% A 100%)",data28)],
+                    ["NOTABLE (90% A 94%)",getDataEstadistica("NOTABLE (90% A 94%)",data28)],
+                    ["BUENO (80% A 89%)",getDataEstadistica("BUENO (80% A 89%)",data28)],
+                    ["SUFICIENTE (70% A 79%)",getDataEstadistica("SUFICIENTE (70% A 79%)",data28)],
+                    ["DEFICIENTE (MENOS DEL 70%)",getDataEstadistica("DEFICIENTE (MENOS DEL 70%)",data28)],
+                ],
+                docentes:[
+                    ["Name", "Los docentes con los que contó tu división de carrera tenían las competencias y capacidades para dotarnos de la formación comprometida"],
+                    ["Muy de acuerdo",getDataEstadistica("Muy de acuerdo",data29)],
+                    ["De acuerdo",getDataEstadistica("De acuerdo",data29)],
+                    ["En desacuerdo",getDataEstadistica("En desacuerdo",data29)],
+                    ["Muy en desacuerdo",getDataEstadistica("Muy en desacuerdo",data29)],
+                ],
+                conociTeor:[
+                    ["Name", "Los profesores estaban al día en el conocimiento teórico y práctico de la disciplina y eso era evidente en sus clases"],
+                    ["Muy de acuerdo",getDataEstadistica("Muy de acuerdo",data30)],
+                    ["De acuerdo",getDataEstadistica("De acuerdo",data30)],
+                    ["En desacuerdo",getDataEstadistica("En desacuerdo",data30)],
+                    ["Muy en desacuerdo",getDataEstadistica("Muy en desacuerdo",data30)],
+                ],
+                persAdm:[
+                    ["Name", "El personal administrativo de tu división de carrera entregaba los servicios adecuados para un funcionamiento eficiente"],
+                    ["Muy de acuerdo",getDataEstadistica("Muy de acuerdo",data31)],
+                    ["De acuerdo",getDataEstadistica("De acuerdo",data31)],
+                    ["En desacuerdo",getDataEstadistica("En desacuerdo",data31)],
+                    ["Muy en desacuerdo",getDataEstadistica("Muy en desacuerdo",data31)],
+                ],
+                persAdm2:[
+                    ["Name", "La cantidad de personal administrativo era la adecuada para proporcionar la atención oportuna y eficiente"],
+                    ["Muy de acuerdo",getDataEstadistica("Muy de acuerdo",data32)],
+                    ["De acuerdo",getDataEstadistica("De acuerdo",data32)],
+                    ["En desacuerdo",getDataEstadistica("En desacuerdo",data32)],
+                    ["Muy en desacuerdo",getDataEstadistica("Muy en desacuerdo",data32)],
+                ],
+                segEgr:[
+                    ["Name", "Existe un proceso eficiente de seguimiento de los egresados en el que he participado"],
+                    ["Muy de acuerdo",getDataEstadistica("Muy de acuerdo",data33)],
+                    ["De acuerdo",getDataEstadistica("De acuerdo",data33)],
+                    ["En desacuerdo",getDataEstadistica("En desacuerdo",data33)],
+                    ["Muy en desacuerdo",getDataEstadistica("Muy en desacuerdo",data33)],
+                ],
+                formacion:[
+                    ["Name", "La formación que recibí, fue suficiente para desempeñar de manera satisfactoria las prácticas profesionales y para enfrentarme al mundo laboral"],
+                    ["Muy de acuerdo",getDataEstadistica("Muy de acuerdo",data34)],
+                    ["De acuerdo",getDataEstadistica("De acuerdo",data34)],
+                    ["En desacuerdo",getDataEstadistica("En desacuerdo",data34)],
+                    ["Muy en desacuerdo",getDataEstadistica("Muy en desacuerdo",data34)],
+                ],
+                contratado:[
+                    ["Name", "Al egresar de la carrera, fui contratado de acuerdo a mis expectativas profesionales y sueldo"],
+                    ["Muy de acuerdo",getDataEstadistica("Muy de acuerdo",data35)],
+                    ["De acuerdo",getDataEstadistica("De acuerdo",data35)],
+                    ["En desacuerdo",getDataEstadistica("En desacuerdo",data35)],
+                    ["Muy en desacuerdo",getDataEstadistica("Muy en desacuerdo",data35)],
+                ],
                 
             })
         }
